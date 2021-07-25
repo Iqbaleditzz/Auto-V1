92663ebf98801143c6bba5e7f52efea19138ff54
IET Updates <83505196+IETUpdates@users.noreply.github.com>
2021-07-25 23:54:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ class Bot(Client):
         self.USER, self.USER_ID = await User().start()
         await self.USER.send_message(
             chat_id=usr_bot_me.username,
-            text="😬🤒🤒"
+            text="Hallo മുതലാളി..😬🤒🤒"
         )
 
     async def stop(self, *args):
