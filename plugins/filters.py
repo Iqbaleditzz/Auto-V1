8e22d0238101979d60379c78c43e5846de3cd8eb
IET Updates <83505196+IETUpdates@users.noreply.github.com>
2021-07-26 11:18:42 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -189,7 +189,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="help_data"),
                     InlineKeyboardButton("START", callback_data="start_data")],
-                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/IETUpdates/Auto-Filter-Bot")]
+                [InlineKeyboardButton("CLOSE 🔒", callback_data='close')]
             ])
 
             await query.message.edit_text(
