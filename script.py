ac28ef76b8b5fcbe39b604d4a205a9c91c3031dc
Iqbaleditzz <89727636+Iqbaleditzz@users.noreply.github.com>
2021-09-08 10:45:44 +0530

    Update script.py

--- script.py
+++ script.py
@@ -12,8 +12,7 @@ For more Don't click <i>/help</i></b>"""
 
 
     HELP_MSG ="""."""
-     bot.send_sticker(chat_id=message.from_user.id, sticker='CAACAgUAAxkBAAIwMmEJE6NKhQYbuL0b__7fG2nOH8M9AAJWAgACz5sZVAGl7RBliJ6jHgQ')
-
+     
 
     ABOUT_MSG = """⭕️<b>My Name : MM Auto Filter Bot</b>
     
