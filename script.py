9fba5999f1150a5be4fdec1774b0100ceb0f8c9d
Iqbaleditzz <89727636+Iqbaleditzz@users.noreply.github.com>
2021-09-08 10:30:52 +0530

    Update script.py

--- script.py
+++ script.py
@@ -11,8 +11,8 @@ You can call this as an Auto Filter Bot of @MM_MOVIESS
 For more Don't click <i>/help</i></b>"""
 
 
-    HELP_MSG = 
-     send_sticker(chat_id=message.from_user.id, sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI')
+    HELP_MSG = """
+     send_sticker(chat_id=message.from_user.id, sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI')"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : MM Auto Filter Bot</b>
